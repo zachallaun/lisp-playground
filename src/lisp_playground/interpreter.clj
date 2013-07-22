b22ce3a0cb690e3be0d41d3393a3d18acc188f79
Zachary Allaun <zach.allaun@gmail.com>
2013-07-22 16:47:40 -0400

    Basic lisp interpreter w/o any environment mutation
    The big missing piece is names: you basically have no way of extending
    the environment except for binding arguments to parameter names.

--- src/lisp_playground/interpreter.clj
+++ src/lisp_playground/interpreter.clj
@@ -1,1 +1,110 @@
-(ns lisp-playground.interpreter)
+(ns lisp-playground.interpreter
+  (:require [clojure.core.match :refer [match]]))
+
+(defprotocol IEnv
+  (lookup [e name]
+    "Looks up `name` in environment `e`. Throws an exception
+     if `name` is not found.")
+  (add [e name val]
+    "Returns a new environment like `e`, but with `name`
+     associated to `val`."))
+
+(defrecord MapEnv [m]
+  IEnv
+  (lookup [e name]
+    (or (get m name)
+        (throw (Exception. (str "Name " name
+                                " not found in environment "
+                                (pr-str e))))))
+  (add [e name val]
+    (->MapEnv (assoc m name val))))
+
+(defn extend-env
+  "Extends an environment using `add` given some associative
+   name/value collection."
+  [e coll]
+  (reduce-kv add e coll))
+
+(def empty-env (->MapEnv {}))
+
+(defn macro?
+  "Returns true if the :macro key of f's metadata is set to true."
+  [f]
+  (= true (:macro (meta f))))
+
+(defn interpret-in-env
+  [expr env]
+  (match [expr]
+    ;; quote form
+    ;;   (quote (1 2 3)) => '(1 2 3)
+    [(['quote e] :seq)] e
+
+    ;; if form
+    ;;   (if (null? ()) 1 0) => 1
+    [(['if pred conseq alt] :seq)]
+    (if (not= () (interpret-in-env pred env))
+      (interpret-in-env conseq env)
+      (interpret-in-env alt env))
+
+    ;; anonymous functions
+    ;;   (f (x) (+ x 1)) => a Clojure function
+    ;;   (f (x y) (cons x (cons y '()))) => also a Clojure function
+    [(['f ([& params] :seq) body] :seq)]
+    (fn [& args]
+      (interpret-in-env body (extend-env env (zipmap params args))))
+
+    ;; anonymous macros
+    ;;   (macro (x) (list '+ 1 x)) => a Clojure function w/ :macro metadata
+    [(['macro ([& params] :seq) body] :seq)]
+    (with-meta (interpret-in-env (list 'f params body) env) {:macro true})
+
+    ;; macroexpansion
+    ;;   '(macroexpand-1 ((macro (x) (list '+ 1 x))) 1) => '(+ 1 1)
+    [(['macroexpand-1 form] :seq)]
+    (if-not (list? form)
+      form
+      (let [[hd & args] form
+            operator (interpret-in-env hd env)]
+        (if-not (macro? operator)
+          form
+          (apply operator args))))
+
+    ;; function and macro application
+    ;;   (+ 1 2) => 3
+    [([function & args] :seq)]
+    (let [f-or-macro (interpret-in-env function env)]
+      (if (macro? f-or-macro)
+        (interpret-in-env (apply f-or-macro args) env)
+        (apply f-or-macro
+               (map #(interpret-in-env % env) args))))
+
+    ;; symbol lookup
+    ;;   foo => whatever foo is bound to, or an error
+    [(sym :guard symbol?)]
+    (lookup env sym)
+
+    ;; numbers eval to themselves
+    ;;   1 => 1
+    ;;   1.0 => 1.0
+    [(n :guard number?)]
+    n
+
+    [_] (throw (Exception. "fail"))))
+
+(def built-ins
+  {'t true
+   'null ()
+   '+ +
+   '- -
+   '/ /
+   '* *
+   'cons cons
+   'car #(or (first %) ())
+   'cdr rest
+   'null? #(or (= % ()) ())
+   'list list
+   '= =})
+
+(defn interpret
+  [expr]
+  (interpret-in-env expr (extend-env empty-env built-ins)))
--- test/lisp_playground/t_interpreter.clj
+++ test/lisp_playground/t_interpreter.clj
@@ -1,3 +1,54 @@
 (ns lisp-playground.t-interpreter
   (:require [midje.sweet :refer :all]
             [lisp-playground.interpreter :refer :all]))
+
+(facts "can interpret expressions"
+
+  (fact "atoms"
+    ;; We're going to define only two atoms: symbols and
+    ;; numbers. Perhaps unfortunately though, interpreting a symbol
+    ;; fails because the default interpretation is an environment
+    ;; lookup. Evaluating to a symbol actually requires '(quote sym),
+    ;; which is not an atom.
+    (interpret 2)    => 2
+    (interpret '2)   => 2
+    (interpret 2.0)  => 2.0
+    (interpret 'foo) => (throws))
+
+  (fact "quoting"
+    (interpret '(quote foo)) => 'foo
+    (interpret '(quote (1 2 3))) => '(1 2 3)
+    (interpret (quote '())) => ())
+
+  (fact "env lookup"
+    (interpret 't) => true
+    (interpret 'null) => '())
+
+  (fact "application"
+    (interpret '(+ 1 2)) => 3
+    (interpret '(+ (- 2 1) (- 3 1))) => 3
+    (interpret '(cons 1 '(2 3))) => '(1 2 3)
+    (interpret '(car '(1 2 3))) => 1
+    (interpret '(cdr '(1 2 3))) => '(2 3)
+    (interpret '(car '())) => '()
+    (interpret '(cdr '())) => '()
+    (interpret '(null? '())) => true
+    (interpret '(null? '(1 2 3))) => '())
+
+  (fact "anonymous functions"
+    (interpret '(f (x) (+ x 1))) => fn?
+    (interpret '((f (x) (+ x 1)) 10)) => 11
+    (interpret '((f (x y) (cons x (cons y '()))) 1 2)) => '(1 2))
+
+  (fact "if"
+    (interpret '(if t 1 0)) => 1
+    (interpret '(if '() 1 0)) => 0
+    (interpret '(if (null? '()) 1 0)) => 1)
+
+  (fact "macros and macroexpand-1"
+    (interpret '(macro (x) (list '+ 1 x))) => (comp :macro meta)
+    (interpret '((macro (x) (list '+ 1 x)) 1)) => 2
+    (interpret '(macroexpand-1 ((macro (x) (list '+ 1 x)) 1))) => '(+ 1 1)
+    (interpret '(macroexpand-1 1)) => 1
+    (interpret '(macroexpand-1 (+ 1 2))) => '(+ 1 2)))
+
